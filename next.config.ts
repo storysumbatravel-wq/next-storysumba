87f23b3f1202d6aa81ce799346d25ef2c751036e
storysumbatravel <storysumbatravel@gmail.com>
2026-06-16 09:20:07 +0800

    redeploy

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,7 @@
-const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "**",
-      },
-    ],
-  },
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
 };
 
 export default nextConfig;
