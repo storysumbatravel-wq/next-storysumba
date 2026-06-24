84cca4f980a84b1273d2538edf95a970b33fb9e0
storysumbatravel <storysumbatravel@gmail.com>
2026-06-24 10:44:41 +0800

    perbaikan 503

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -76,7 +76,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" data-scroll-behavior="smooth">
       <body>
         <LanguageProvider>
           <Navbar />
